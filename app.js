f371816e5dc6a70ca918d1932e7c011c7f1595b4
Jamie Schrifrin <jamieschifrin@hotmail.com>
2020-07-10 08:57:13 -0400

    homework

---
+++ app.js
@@ -0,0 +1,22 @@
+var ourArray = [];
+for (var i = 0; i < 10; i++) {
+  ourArray.push(i);
+}
+
+
+var ourArray = [];
+var i = 0;
+while(i < 10) {
+  ourArray.push(i);
+  i--;
+}
+
+var ourArray = [];
+for (var i = 11; i < 22; i += 2) {
+  ourArray.push(i);
+}
+
+var ourArray = [];
+for (var i =  > 0; i -= 2) {
+  ourArray.push(i);
+}
